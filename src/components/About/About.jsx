96e5ffaa80b2e837f08358636b770e5b8b7155da
MuhammadazizEsonboyev <esonboyev03@bk.ru>
2023-05-11 16:25:08 +0500

    fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
-function App() {
+import { Col, Container, ListGroup, Row } from 'react-bootstrap'
+import axios from 'axios'
+
+function About () {
+  const [data, setData] = useState([])
+
+  const getData = () => {
+    axios.get('http://localhost:9999/posts').then(res => {
+      setData(res.data)
+    })
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const DeleteData = id => {
+    axios.delete(`http://localhost:9999/posts/${id}`).then(res => {
+      setData(data?.filter(el => el.id !== id))
+      alert(res.statusText)
+    })
+  }
+
   return (
     <>
-    <header>
+      <header>
         <h1>Hello About Page</h1>
-        <p className='text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas dolore dolorem perferendis vitae cupiditate, iusto laborum dolorum. Itaque, laborum tempore dolore excepturi labore corporis architecto, est ea quis amet iusto cum. Reprehenderit illo iste eum, corrupti provident natus fugiat ducimus ab deserunt ad nisi est voluptatibus. Voluptatum, expedita. Dolore adipisci eum provident voluptates, debitis quibusdam ad aut. Ab dolorum nam unde fugiat voluptas deleniti, quas officia vero similique, consequuntur quos aperiam, nemo expedita exercitationem repudiandae maxime architecto reiciendis laborum minus amet voluptate aliquam recusandae. Exercitationem assumenda quia, accusantium eius placeat similique accusamus laboriosam excepturi, necessitatibus provident atque laudantium suscipit sint!</p>
-    </header>
+        <Container>
+          <Row className='justify-content-center'>
+            <Col lg={7}>
+              <ListGroup>
+                {data?.map(item => (
+                  <ListGroup.Item
+                    key={item.id}
+                    style={{
+                      textAlign: 'start',
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center'
+                    }}
+                  >
+                    {item?.username}
+                    <div className='btnDiv'>
+                      <button
+                        onClick={() => DeleteData(item.id)}
+                        style={{ background: 'transparent' }}
+                      >
+                        <i
+                          style={{ color: 'black', fontSize: '26px' }}
+                          className='fa-sharp fa-solid fa-trash'
+                        ></i>
+                      </button>
+                    </div>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Col>
+          </Row>
+        </Container>
+      </header>
     </>
-  );
+  )
 }
 
-export default App;
+export default About
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,158 +1,59 @@
-// import React from "react";
-// import PropTypes from "prop-types";
-// import "./contact.css";
+import { useState } from 'react'
+import './contact.css'
+import FormInput from '../FormInput'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
-// class ContactForm extends React.Component {
-//   static propTypes = {
-//     name: PropTypes.string.isRequired,
-//     email: PropTypes.string.isRequired,
-//     message: PropTypes.string.isRequired
-//   };
-
-//   state = {
-//     name: "",
-//     email: "",
-//     message: ""
-//   };
-
-//   handleInputChange = event => {
-//     const target = event.target;
-//     const value = target.value;
-//     const name = target.name;
-
-//     this.setState({
-//       [name]: value
-//     });
-//   };
-
-//   handleSubmit = event => {
-//     event.preventDefault();
-//     console.log("Form submitted with values:", this.state);
-//     // Submit form code here
-//   };
-
-//   render() {
-//     return (
-//       <form onSubmit={this.handleSubmit}>
-//         <div>
-//           <label htmlFor="name">Name:</label>
-//           <input
-//             type="text"
-//             id="name"
-//             name="name"
-//             required
-//             value={this.state.name}
-//             onChange={this.handleInputChange}
-//           />
-//         </div>
-//         <div>
-//           <label htmlFor="email">Email:</label>
-//           <input
-//             type="email"
-//             id="email"
-//             name="email"
-//             required
-//             value={this.state.email}
-//             onChange={this.handleInputChange}
-//           />
-//         </div>
-//         <div>
-//           <label htmlFor="message">Message:</label>
-//           <textarea
-//             id="messzage"
-//             name="message"
-//             required
-//             value={this.state.message}
-//             onChange={this.handleInputChange}
-//           />
-//         </div>
-//         <button type="submit">Submit</button>
-//       </form>
-//     );
-//   }
-// }
-
-// export default ContactForm;
-
-
-
-import { useState } from "react";
-import "./contact.css";
-import FormInput from "../FormInput";
-
-  function Contact() {
+function Contact () {
   const [values, setValues] = useState({
-    username: "",
-    email: "",
-    birthday: "",
-    password: "",
-    confirmPassword: "",
-  });
+    username: '',
+    email: ''
+  })
+
+  const navigate = useNavigate()
+  const PostData = () => {
+    axios.post('http://localhost:9999/posts', values).then(res => {
+      alert(res.statusText)
+      navigate('/')
+    })
+  }
 
   const inputs = [
     {
       id: 1,
-      name: "username",
-      type: "text",
-      placeholder: "Username",
+      name: 'username',
+      type: 'text',
+      placeholder: 'Username',
       errorMessage:
         "Username should be 3-16 characters and shouldn't include any special character!",
-      label: "Username",
-      pattern: "^[A-Za-z0-9]{3,16}$",
-      required: true,
+      label: 'Username',
+      pattern: '^[A-Za-z0-9]{3,16}$',
+      required: true
     },
     {
       id: 2,
-      name: "email",
-      type: "email",
-      placeholder: "Email",
-      errorMessage: "It should be a valid email address!",
-      label: "Email",
-      required: true,
-    },
-    {
-      id: 3,
-      name: "birthday",
-      type: "date",
-      placeholder: "Birthday",
-      label: "Birthday",
-    },
-    {
-      id: 4,
-      name: "password",
-      type: "password",
-      placeholder: "Password",
-      errorMessage:
-        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
-      label: "Password",
-      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
-      required: true,
-    },
-    {
-      id: 5,
-      name: "confirmPassword",
-      type: "password",
-      placeholder: "Confirm Password",
-      errorMessage: "Passwords don't match!",
-      label: "Confirm Password",
-      pattern: values.password,
-      required: true,
-    },
-  ];
+      name: 'email',
+      type: 'email',
+      placeholder: 'Email',
+      errorMessage: 'It should be a valid email address!',
+      label: 'Email',
+      required: true
+    }
+  ]
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
 
-  const onChange = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  const onChange = e => {
+    setValues({ ...values, [e.target.name]: e.target.value })
+  }
 
   return (
-    <div className="app">
+    <div className='app'>
       <form onSubmit={handleSubmit}>
         <h1>Register</h1>
-        {inputs.map((input) => (
+        {inputs.map(input => (
           <FormInput
             key={input.id}
             {...input}
@@ -160,10 +61,10 @@ import FormInput from "../FormInput";
             onChange={onChange}
           />
         ))}
-        <button>Submit</button>
+        <button onClick={() => PostData()}>Submit</button>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default Contact;
+export default Contact
